456504863d1da4b6879448a0b9dbf8c8434901fd
Emir Tunahan Alim <emrtnhalim@gmail.com>
2026-08-30 16:38:17 +0300

    MIHVER: add nonlinear invariant value head

--- src/harbichess/backends/decoupled_value_network.py
+++ src/harbichess/backends/decoupled_value_network.py
@@ -24,13 +24,16 @@ class HarbiChessDecoupledValueNetwork(HarbiChessInvariantValueNetwork):
         invariant_config: InvariantValueConfig | None = None,
     ) -> None:
         super().__init__(config, invariant_config=invariant_config)
-        self.material_value_linear = nn.Linear(
-            PIECE_PLANES_PER_STEP + METADATA_PLANES, 1
-        )
-        self.material_value_linear.weight = mx.zeros_like(
-            self.material_value_linear.weight
-        )
+        self.material_value_linear = nn.Linear(PIECE_PLANES_PER_STEP + METADATA_PLANES, 1)
+        self.global_value_hidden = nn.Linear(PIECE_PLANES_PER_STEP + METADATA_PLANES, 64)
+        self.global_value_output = nn.Linear(64, 3)
+        self.material_value_linear.weight = mx.zeros_like(self.material_value_linear.weight)
         self.material_value_linear.bias = mx.zeros_like(self.material_value_linear.bias)
+        self._zero_global_value_output()
+
+    def _zero_global_value_output(self) -> None:
+        self.global_value_output.weight = mx.zeros_like(self.global_value_output.weight)
+        self.global_value_output.bias = mx.zeros_like(self.global_value_output.bias)
 
     @classmethod
     def from_base(
@@ -42,28 +45,26 @@ class HarbiChessDecoupledValueNetwork(HarbiChessInvariantValueNetwork):
         target = cls(base.config, invariant_config=invariant_config)
         target.load_weights(list(tree_flatten(base.parameters())), strict=False)
         target._zero_residual_outputs()
-        target.material_value_linear.weight = mx.zeros_like(
-            target.material_value_linear.weight
-        )
-        target.material_value_linear.bias = mx.zeros_like(
-            target.material_value_linear.bias
-        )
+        target.material_value_linear.weight = mx.zeros_like(target.material_value_linear.weight)
+        target.material_value_linear.bias = mx.zeros_like(target.material_value_linear.bias)
+        target._zero_global_value_output()
         mx.eval(target.parameters())
         return target
 
     @staticmethod
     def invariant_features(inputs: mx.array) -> mx.array:
-        current_piece_counts = mx.sum(
-            inputs[:, :, :, :PIECE_PLANES_PER_STEP], axis=(1, 2)
-        )
+        current_piece_counts = mx.sum(inputs[:, :, :, :PIECE_PLANES_PER_STEP], axis=(1, 2))
         metadata_start = HISTORY_STEPS * PIECE_PLANES_PER_STEP
         metadata = mx.mean(inputs[:, :, :, metadata_start:], axis=(1, 2))
         return mx.concatenate((current_piece_counts, metadata), axis=1)
 
     def material_value(self, inputs: mx.array) -> mx.array:
-        return mx.tanh(self.material_value_linear(self.invariant_features(inputs))).squeeze(
-            axis=1
-        )
+        return mx.tanh(self.material_value_linear(self.invariant_features(inputs))).squeeze(axis=1)
+
+    def _value_residual(self, inputs: mx.array) -> mx.array:
+        residual = super()._value_residual(inputs)
+        hidden = nn.relu(self.global_value_hidden(self.invariant_features(inputs)))
+        return residual + self.global_value_output(hidden)
 
     def freeze_to_material_head(self) -> None:
         self.freeze()
@@ -72,6 +73,10 @@ class HarbiChessDecoupledValueNetwork(HarbiChessInvariantValueNetwork):
     def freeze_to_global_wdl(self) -> None:
         self.freeze()
         self.invariant_value_linear.unfreeze()
+        self.global_value_hidden.unfreeze()
+        self.global_value_output.unfreeze()
 
     def freeze_to_global_tower_wdl(self) -> None:
         self.freeze_release_parameters()
+        self.global_value_hidden.unfreeze()
+        self.global_value_output.unfreeze()
